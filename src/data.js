a9f7e992408cc65b9449ac4451d24417bbc243c6
smartkittydev <smartkitty1218@gmail.com>
2022-06-30 10:57:47 +0300

    change letter

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 0,
-    label: "Native American",
+    label: "aaa",
     value: 1,
     color: "#0089cf"
   },
   {
     id: 1,
-    label: "2+ Races",
+    label: "bbb",
     value: 3,
     color: "#0db14b"
   },
   {
     id: 2,
-    label: "Asian American",
+    label: "ccc",
     value: 7,
     color: "#fdb913"
   },
   {
     id: 3,
-    label: "Hispanic",
+    label: "ddd",
     value: 15,
     color: "#f36f21"
   },
   {
     id: 4,
-    label: "African American",
+    label: "eee",
     value: 19,
     color: "#c9234a"
   },
   {
     id: 5,
-    label: "White",
+    label: "fff",
     value: 45,
     color: "#645faa"
   }
